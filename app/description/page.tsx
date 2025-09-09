402442b64c8f0df3006e7d1f0ae7906e3006c78e
Heksa R <trisnawan987@gmail.com>
2025-09-09 08:47:07 +0700

    Navbar fixed

---
+++ app/description/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Navbar from "@/components/navbar";
+export default function desc() {
+    return (
+        <div className="min-h-screen bg-gray-50">
+            {/* Navbar */}
+            <Navbar />
+            
+        </div>
+    );
+}
