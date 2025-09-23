43db32d99301219f1d24f2c4eb1eb6aa4e1c0e57
Satyaa_23 <153822550+satyaa232323@users.noreply.github.com>
2025-09-23 11:36:32 +0700

    refactor: simplify GET function in route.ts by removing redundant code

--- app/api/classes/route.ts
+++ app/api/classes/route.ts
@@ -1,46 +1,27 @@
 import { PrismaClient } from "@/app/generated/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
-  const prisma = new PrismaClient();
+export async function GET() {
+    const prisma = new PrismaClient();
 
-  try {
-
-    const classes = await prisma.class.findMany({
-      select: {
-        id: true,
-        thumbnailUrl: true,
-        title: true,
-        description: true,
-        price: true,
-        mentor: true,
-      },
-    });
-    return NextResponse.json({ data: classes }, { status: 200 });
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json(
-      { message: "Internal server error" },
-      { status: 500 }
-    );
-  }
-    const classes = await prisma.class.findMany({
-      select: {
-        id: true,
-        thumbnailUrl: true,
-        title: true,
-        description: true,
-        price: true,
-        mentor: true,
-      },
-    });
-    return NextResponse.json({ data: classes }, { status: 200 });
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json(
-      { message: "Internal server error" },
-      { status: 500 }
-    );
-  }
+    try {
+        const classes = await prisma.class.findMany({
+            select: {
+                id: true,
+                thumbnailUrl: true,
+                title: true,
+                description: true,
+                price: true,
+                mentor: true,
+            },
+        });
+        return NextResponse.json({ data: classes }, { status: 200 });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json(
+            { message: "Internal server error" },
+            { status: 500 }
+        );
+    }
 }
 
