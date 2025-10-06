f573f9cb8bd9bbb0ffe1fd151f70006fcb84099c
Heksa R <trisnawan987@gmail.com>
2025-10-06 08:33:05 +0700

    Beli lewat halaman classes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -78,7 +78,18 @@ const Navbar: React.FC<NavbarProps> = ({ onSearchChange }) => {
             <Link href="/profile">
               <div className="w-10 h-10 rounded-full bg-red-500 flex items-center justify-center cursor-pointer">
                 <span className="text-white font-bold text-lg">
-                  {user.name ? user.name.charAt(0).toUpperCase() : ""}
+                  {user.name ? (
+                    user.name.charAt(0).toUpperCase()
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="white"
+                      viewBox="0 0 24 24"
+                      className="w-7 h-7 text-white"
+                    >
+                      <path d="M15.75 7.5a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.5 19.5a7.5 7.5 0 0115 0v.75a.75.75 0 01-.75.75h-13.5a.75.75 0 01-.75-.75V19.5z" />
+                    </svg>
+                  )}
                 </span>
               </div>
             </Link>
@@ -104,7 +115,18 @@ const Navbar: React.FC<NavbarProps> = ({ onSearchChange }) => {
             <Link href="/profile">
               <div className="w-10 h-10 rounded-full bg-red-500 flex items-center justify-center">
                 <span className="text-white font-bold text-lg">
-                  {user.name ? user.name.charAt(0).toUpperCase() : "U"}
+                  {user.name ? (
+                    user.name.charAt(0).toUpperCase()
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="white"
+                      viewBox="0 0 24 24"
+                      className="w-7 h-7 text-white"
+                    >
+                      <path d="M15.75 7.5a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.5 19.5a7.5 7.5 0 0115 0v.75a.75.75 0 01-.75.75h-13.5a.75.75 0 01-.75-.75V19.5z" />
+                    </svg>
+                  )}
                 </span>
               </div>
             </Link>
