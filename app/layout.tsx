70c177aef1d7ee7b5414d52801c5469883f72f75
ByanHasyifPulinggaNurprianto24 <byanhasyif24@gmail.com>
2025-10-06 09:59:45 +0700

    hapus footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ToastProvider } from "@/components/ToastContext";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -39,6 +40,10 @@ export default function RootLayout({
       >
         <ToastProvider>
           {children}
+          {/* Footer*/}
+          <div>
+            <Footer />
+          </div>
         </ToastProvider>
       </body>
     </html>
